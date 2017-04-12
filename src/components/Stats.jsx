c874e90893455e8c096173f1406812f1d2fb431d
Nigen <Nigen48@gmail.com>
2017-04-12 13:27:45 +0300

    Исправление синтакс. ошибки

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Stats(props) {
   let total = props.todos.length;
   let completed = props.todos.filter(todo => todo.completed).length;
-  let notCompleted = toal - completed;
+  let notCompleted = total - completed;
 
     return (
       <table className="stats">
